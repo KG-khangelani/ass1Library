33438130f00894d26a629ca49f056116627bdb44
Kaygizzle <62054578+KG-khangelani@users.noreply.github.com>
2025-05-20 16:27:58 +0200

    implement base class

--- libraryitem.cpp
+++ libraryitem.cpp
@@ -1,3 +1,33 @@
 #include "libraryitem.h"
+#include <QString>
+
 
 LibraryItem::LibraryItem() {}
+
+QString LibraryItem::getTitle() {
+    return this->title;
+}
+
+QString LibraryItem::getAuthor() {
+    return this->author;
+}
+
+int LibraryItem::getID() {
+    return this->id;
+}
+
+bool LibraryItem::getIsBorrowed() {
+    return this -> isBorrowed;
+}
+
+void LibraryItem::setAuthor(QString fullname){
+    this -> author = fullname;
+}
+
+void LibraryItem::setBorrowing(bool value) {
+    this -> isBorrowed = value;
+}
+
+void LibraryItem::setTitle(QString value) {
+    this -> title = value;
+}
--- libraryitem.h
+++ libraryitem.h
@@ -6,13 +6,22 @@ class LibraryItem
 {
 public:
     LibraryItem();
+    int getID();
     QString getTitle();
     QString getAuthor();
     bool getIsBorrowed();
 
-    void setAuthor(QString value);
+    void setAuthor(QString fullname);
     void setTitle(QString value);
     void setBorrowing(bool value);
+
+    friend bool operator==(const LibraryItem& lhs, const LibraryItem& rhs) {
+        return lhs.title == rhs.title &&
+               lhs.author == rhs.author &&
+               lhs.id == rhs.id;
+    };
+
+    virtual void displayInfo();
 private:
     int id;
     QString title;
